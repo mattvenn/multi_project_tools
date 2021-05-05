7c6c3aa9ae04eedb788f46ddec911d9f61ba7209
Matt Venn <matt@mattvenn.net>
2021-05-05 15:39:22 +0200

    changes for mpw-two-b caravel

--- module_instance.v
+++ module_instance.v
@@ -25,12 +25,15 @@
         // only provide first 32 bits to reduce wiring congestion
         .la_data_in (la_data_in [31:0]),
         .la_data_out(la_data_out[31:0]),
-        .la_oen     (la_oenb[31:0]),
+        .la_oenb    (la_oenb[31:0]),
 
         // IOs
         .io_in      (io_in),
         .io_out     (io_out),
         .io_oeb     (io_oeb),
+
+        // IRQs
+        .irq        (user_irq),
         
         // active input, only connect tristated outputs if this is high
         .active     (la_data_in[32+PROJECT_ID])
