ca9483b46cbb033c7b8e911f42c6b22562d2daaf
Matt Venn <matt@mattvenn.net>
2022-03-04 15:36:05 +0100

    update config

--- config.tcl
+++ config.tcl
@@ -16,6 +16,9 @@
 # Base Configurations. Don't Touch
 # section begin
 
+set ::env(PDK) "sky130A"
+set ::env(STD_CELL_LIBRARY) "sky130_fd_sc_hd"
+
 # YOU ARE NOT ALLOWED TO CHANGE ANY VARIABLES DEFINED IN THE FIXED WRAPPER CFGS 
 source $::env(CARAVEL_ROOT)/openlane/user_project_wrapper/fixed_wrapper_cfgs.tcl
 
@@ -35,10 +38,9 @@ set ::env(RUN_KLAYOUT_DRC) 0
 # no point in running DRC with magic once openram is in because it will find 3M issues
 set ::env(MAGIC_DRC_USE_GDS) 0
 
-
 ## Source Verilog Files
 set ::env(VERILOG_FILES) "\
-	$script_dir/../../caravel/verilog/rtl/defines.v \
+    $script_dir/../../caravel/verilog/rtl/defines.v \
 	$script_dir/../../verilog/rtl/user_project_wrapper.v"
 
 ## Clock configurations
@@ -57,35 +59,25 @@ set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
 
 ### Black-box verilog and views
 set ::env(VERILOG_FILES_BLACKBOX) "\
-	$script_dir/../../caravel/verilog/rtl/defines.v \
+    $script_dir/../../caravel/verilog/rtl/defines.v \
 	$script_dir/../../verilog/rtl/user_project_includes.v"
 
+### user projects gds and lef files
 set ::env(EXTRA_LEFS) [glob $::env(DESIGN_DIR)/macros/lef/*.lef]
-
 set ::env(EXTRA_GDS_FILES) [glob $::env(DESIGN_DIR)/macros/gds/*.gds]
 
 # these get generated - if a project specifies obstruction in the info.yaml
 source user_project_wrapper/obstruction.tcl
 
-set ::env(GLB_RT_ALLOW_CONGESTION) "1"
-
-#Reduction in the routing capacity of the edges between the cells in the global routing graph. Values range from 0 to 1.
-#1 = most reduction, 0 = least reduction 
-set ::env(GLB_RT_ADJUSTMENT) 0.70
-
-# per layer adjustment
-# 0 -> 1: 1 means don't use the layer                                                        
-# l2 is met1                                                                                 
-set ::env(GLB_RT_L2_ADJUSTMENT) 0.9
-set ::env(GLB_RT_L3_ADJUSTMENT) 0.7
 
 # use 8 cores
 set ::env(ROUTING_CORES) 8
 
 # bail early on problems
-set ::env(ROUTING_OPT_ITERS) 30
+set ::env(DRT_OPT_ITERS) 30
 
-set ::env(GLB_RT_MAXLAYER) 5
+# set ::env(GLB_RT_MAXLAYER) 5
+set ::env(RT_MAX_LAYER) {met4}
 
 # disable pdn check nodes becuase it hangs with multiple power domains.
 # any issue with pdn connections will be flagged with LVS so it is not a critical check.
